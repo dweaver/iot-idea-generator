96882d584a12b1726f16de1a1767177c2215568c
Dan Weaver <danweaver@exosite.com>
2015-11-03 20:20:01 -0600

    use JWT for own domain

--- generator.js
+++ generator.js
@@ -86,7 +86,7 @@ $( document ).ready(function () {
         ]
       }] 
     });
-  if (true) {
+  if (false) {
     // example.exohack.io settings
     TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkb21haW4iOiJodHRwczovL2V4b2hhY2suaW8iLCJleHAiOjE0NDYyMjUyMDMsImlhdCI6MTQ0NjIxODAwMywibmFtZSI6ImV4YW1wbGUiLCJuYmYiOjE0NDYyMTgwMDJ9.R8pInT-85tAkcHJ0hcdo2hA3n1pZWOID4xwMFm-5lgg';
     CIK = '36aa36b52277b2273b4fcf1a3fba8e1c61255702';
