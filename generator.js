7a297104eb692b9f5141589c778abcef6c037999
Dan Weaver <danweaver@exosite.com>
2015-11-03 19:45:17 -0600

    style

--- generator.js
+++ generator.js
@@ -6,7 +6,7 @@ $( document ).ready(function () {
   var pluralizable_nouns = [
     'waffle iron', 'laser cutter', 'windmill', 
     'crib', 'thermostat', 'unicycle', 
-    'toaster', 'broiler', 'light-switch', 'hot-tub',
+    'toaster', 'broiler', 'light switch', 'hot tub',
     'holiday lights', 'carwash', 'rice cooker',
     'treadmill', 'stationary-bicycle', 'blender',
     'billboard', 'hair dryer', 'curling iron',
@@ -71,5 +71,44 @@ $( document ).ready(function () {
     project = pluralize(noun, 2) + ' but with like ' + choice(vague_iot_nouns) + ' or something';
   }
   $('#project').html(project);
-  //$('#noun').html(noun);
+  var CIK = '301bd1c9cc9cd86caa3b65d2df961f55733610b2';
+  var CID = '6ea5ecf4f69b732eee4de52044292dc693331862';
+  var TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiaWRlYXMiLCJkb21haW4iOiJodHRwczovL2V4b2hhY2suaW8ifQ.zpAmoTu6Jc6p73Z0FmOwnqq8qQ56fNsJUEzhxUSUaqM';
+  var data = JSON.stringify({
+      auth: {cik: CIK, client_id: CID},
+      calls: [{
+        id: 1,
+        procedure: 'write',
+        arguments: [
+          { alias: 'ideas' },
+          project
+        ]
+      }] 
+    });
+  if (true) {
+    // example.exohack.io settings
+    TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkb21haW4iOiJodHRwczovL2V4b2hhY2suaW8iLCJleHAiOjE0NDYyMjUyMDMsImlhdCI6MTQ0NjIxODAwMywibmFtZSI6ImV4YW1wbGUiLCJuYmYiOjE0NDYyMTgwMDJ9.R8pInT-85tAkcHJ0hcdo2hA3n1pZWOID4xwMFm-5lgg';
+    CIK = '36aa36b52277b2273b4fcf1a3fba8e1c61255702';
+    CID = 'f76cc57a106b479c86341516c8c3aa180445e1ac';
+    data = '{"auth":{"cik":"36aa36b52277b2273b4fcf1a3fba8e1c61255702","client_id":"f76cc57a106b479c86341516c8c3aa180445e1ac"},"calls":[{"procedure":"info","arguments":["cdca21552437ae4349c8f003be6bcecdf5ab2ef9",{"description":true,"key":true,"tags":true,"basic":true,"subscribers":true,"shares":true,"aliases":true}],"id":1},{"procedure":"read","arguments":["cdca21552437ae4349c8f003be6bcecdf5ab2ef9",{"limit":1}],"id":2}]}';
+  }
+  $.ajax({
+    type: 'POST',
+    url: 'https://api.exohack.io/api/onep:v1/rpc/process', 
+    data: data,
+    processData: false,
+    contentType: 'application/json;charset=UTF-8',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + TOKEN
+    },
+    success: function(data) {
+      console.log('data', data); 
+    },
+    error: function(jsxhr, err) {
+      console.log('error', err, jsxhr);
+    }
+  });
+
 });
